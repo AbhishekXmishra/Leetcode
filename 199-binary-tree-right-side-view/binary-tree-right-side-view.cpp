244f62fb82eb4f01ed1969e46ab7bbb6bd42c03d
AbhishekXmishra <153223408+AbhishekXmishra@users.noreply.github.com>
2024-04-21 01:01:29 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (26.96%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,31 +1,22 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void printLeftView(TreeNode* root, int level, vector<int>& leftView) {
-  if(root == NULL) 
-	return ;
-
-  if(level == leftView.size()) {
-	//iska matlab left view ki node milgyi h , store karlo
-	leftView.push_back(root-> val);
-  }
-  printLeftView(root->right, level+1, leftView);
-  printLeftView(root->left, level+1, leftView);
-}
-
     vector<int> rightSideView(TreeNode* root) {
-         vector<int> leftView;
-         printLeftView(root ,0, leftView);
-         return leftView;
+    queue<TreeNode*> q;
+    q.push(root);
+    vector<int> vect;
+    if(root==NULL) return vect;
+    while(!q.empty()){
+        int s=q.size();
+        for(int i=0;i<s;i++){
+            TreeNode* node=q.front();
+            q.pop();
+            if(i==s-1){
+                vect.push_back(node->val);   
+            }
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+        }
+    }
+    return vect;
     }
 };
