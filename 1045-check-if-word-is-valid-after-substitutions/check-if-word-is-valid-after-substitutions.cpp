3db52e3839a28818e392ea0f819e987ce70d33df
AbhishekXmishra <153223408+AbhishekXmishra@users.noreply.github.com>
2024-07-13 01:16:16 +0530

    Time: 8 ms (88.02%) | Memory: 9.7 MB (52.98%) - LeetSync

---
+++ 1045-check-if-word-is-valid-after-substitutions/check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int i = 0, n = s.size();
+        for (int j = 0; j < n; j++) {
+            s[i++] = s[j];
+            if (i >= 3 && s[i - 1] == 'c' && s[i - 2] == 'b' && s[i - 3] == 'a') {
+                i -= 3;
+            }
+        }
+        return i == 0;
+    }
+};
